12aebf39ac7390ea112bfce5bf9be3948f9e355c
Andrew Pham <151401760+AndrewPortfolio@users.noreply.github.com>
2026-08-11 18:54:24 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #array of strings 
+        #anagram = same length and same chars 
+        #return anagrams grouped in any order
+
+        #create a dict to store strings in group 
+        res = defaultdict(list)
+
+        #all strings in strs 
+        for s in strs:
+            count = [0] * 26 #empty array of lowercase alphabet a-z 
+            #each char in string s
+            for c in s:
+                #inc count + 1 for each letter found 
+                count[ord(c) - ord('a')] += 1
+
+            res[tuple(count)].append(s)
+
+        return list(res.values())
