d9e21c48fee749d52b2fb89eb47cd5f2769a4fdb
Andrew Pham <151401760+AndrewPortfolio@users.noreply.github.com>
2026-08-26 18:21:05 -0700

    Add: duplicate-integer - submission-20

---
+++ Data Structures & Algorithms/duplicate-integer/submission-20.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        #if there is a duplicate return true 
+        #ds to store the seen values, if another value inside nums is alr in seen then return true 
+
+        #empty arr
+        # seen = []
+
+        # for n in nums:
+        #     if n in seen:
+        #         return True 
+        #     seen.append(n)
+        
+        # return False
+        return len(set(nums)) != len(nums)
